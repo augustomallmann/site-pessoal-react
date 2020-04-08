66c2e555e5e705fc2ed453aa8bce09136976036a
augustomallmann <augustocmallmann@gmail.com>
2020-04-07 21:35:08 -0300

    production

---
+++ build/precache-manifest.f9b19ffb900595168ac49fac9c0f6266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31724bd2e47dad3590cd43374cc62934",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5a32f4e17f75344247f",
+    "url": "/static/css/main.a4f139a5.chunk.css"
+  },
+  {
+    "revision": "b34fe4210fd8e0cb9fbc",
+    "url": "/static/js/2.1df80d03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1df80d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5a32f4e17f75344247f",
+    "url": "/static/js/main.42440ee7.chunk.js"
+  },
+  {
+    "revision": "9095ac9020844e80805e",
+    "url": "/static/js/runtime-main.f6c48584.js"
+  },
+  {
+    "revision": "973ace5e5c44d8265beb4d990514e586",
+    "url": "/static/media/perfil.973ace5e.jpg"
+  }
+]);
